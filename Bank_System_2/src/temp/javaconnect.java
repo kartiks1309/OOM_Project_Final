a1f0b005ca1d19351fcac71dac69210916c8531c
kartiks1309 <kartik.s1309@gmail.com>
2021-11-12 23:49:53 +0530

    Initial Commit

---
+++ Bank_System_2/src/temp/javaconnect.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temp;
+
+/**
+ *
+ * @author Kartik
+ */
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class javaconnect {
+    
+    Connection conn=null;
+    
+    public static Connection ConnecrDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:D:\\Bank_System_2\\bankx.sqlite");
+            return conn;
+        }catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+           
+        }
+        return null;
+    }
+}
